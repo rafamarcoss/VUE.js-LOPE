cfb2e789fcd7c3ba0e255accbd883a793e518202
rafamarcoss <rafaelmarcos2604@gmail.com>
2025-02-17 11:37:28 +0100

    Update 17 feb

--- project/src/Home.vue
+++ project/src/Home.vue
@@ -1,14 +1,42 @@
 <template>
-    <h1>{{msg}}</h1>
+  <div id="home">
+    <h1>{{ msg }}</h1>
+    <div id="cards-container">
+      <div class="card" v-for="(asignatura, index) in asignaturas" :key="index">
+        <img :src="asignatura.imagen_url" class="card-img" />
+        <h2 class="nombre">{{ asignatura.nombre }}</h2>
+        <h2 class="profesor">{{ asignatura.profesor }}</h2>
+        <h2 class="descripcion">{{ asignatura.descripcion }}</h2>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: 'Home',
+  name: "Home",
   data() {
     return {
-      msg: 'Página HOME',
+      msg: "Asignaturas 1º DAM",
+      asignaturas: []
+    };
+  },
+  mounted() {
+    this.get_asignaturas();
+  },
+  methods: {
+    get_asignaturas() {
+      axios.get("http://192.168.1.50/mi-proyecto/api/get_asignaturas.php")
+    .then(response => {
+    this.asignaturas = response.data.data;
+    })
+    .catch(error => {
+      console.error("Error al obtener los datos:", error);
+    });
+
     }
   }
-}
+};
 </script>
